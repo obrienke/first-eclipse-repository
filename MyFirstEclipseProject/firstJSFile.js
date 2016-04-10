6375f5e21eab6f46298ee1c0f53de218227a0120
obrienke <kevin.obrien@gmit.ie>
2016-04-10 20:07:37 +0100

    second commit

--- MyFirstEclipseProject/firstJSFile.js
+++ MyFirstEclipseProject/firstJSFile.js
@@ -6,7 +6,7 @@ function setText()
 {
 	var x = 1;
 	if(x==0){
-		document.getElementById("demo").innerHTML='Text set by a javascript function';
+		document.getElementById("demo").innerHTML='Text set by a javascript function - test 2';
 	}
 }	
 
