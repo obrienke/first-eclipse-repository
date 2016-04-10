3bbfae3a8f48f8c347ead48329c3f19f3063c35e
obrienke <kevin.obrien@gmit.ie>
2016-04-10 17:24:51 +0100

    test first commit

---
+++ firstEclipseProject/firstJSFile.js
@@ -0,0 +1,11 @@
+/**
+ * My first JavaScript file on Eclipse 
+ */
+
+function setText()
+{
+	var x = 1;
+	if(x==0){
+		document.getElementById("demo").innerHTML='Text set by a javascript function';
+	}
+}		
